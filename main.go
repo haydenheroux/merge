b7cd1b517ef6fb36fab37351f5b6fe91f81865b2
Hayden Heroux <haydenheroux@gmail.com>
2026-03-26 14:47:42 -0400

    Change default port to 8080.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	s := Server{
 		BaseURL: "https://www.merge.zone",
-		Port: 3000,
+		Port: 8080,
 		Router: mux.NewRouter(),
 		Logger: slog.New(slog.NewTextHandler(os.Stderr, nil)),
 		GitHub: &GitHub{
