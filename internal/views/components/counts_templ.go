f1f4d7153bee12c0cde402128922954ef8a2cb45
Hayden Heroux <haydenheroux@gmail.com>
2026-06-28 07:57:01 -0400

    Refactor heading class.

--- internal/views/components/counts_templ.go
+++ internal/views/components/counts_templ.go
@@ -33,7 +33,7 @@ func Counts(counts model.ExpiryCounts) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"stats\"><p class=\"section-label\">Overview</p><div class=\"stats-tiles\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"stats\"><div class=\"section-heading\"><span>Overview</span></div><div class=\"stats-tiles\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- internal/views/components/scopes_templ.go
+++ internal/views/components/scopes_templ.go
@@ -31,7 +31,7 @@ func Scopes(scopes []model.ScopeInfo) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"scopes\"><p class=\"section-label\">Scopes</p><div class=\"scopes-container\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"scopes\"><div class=\"section-heading\"><span>Scopes</span></div><div class=\"scopes-container\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
